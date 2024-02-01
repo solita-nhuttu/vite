7a469caa819f64a766665fdecb612df80ac34f65
Niko Huttu <niko.huttu@solita.fi>
2024-02-01 10:58:00 +0200

    git p

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more eeee
+        Click on the Vite and React logos t12eqwdo learn more eeee
       </p>
     </>
   )
