8959020a9cb083e4defc829eb55730ef493be1a0
Niko Huttu <niko.huttu@solita.fi>
2024-03-21 10:07:01 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    (async function () {
+      const { text } = await( await fetch(`/api/message`)).json();
+      setData(text);
+    })();
+  });
 
   return (
     <>
@@ -29,6 +37,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos t12eqwdo learn more eeee
       </p>
+      <>{data}</>
     </>
   )
 }
